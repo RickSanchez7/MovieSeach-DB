4ad36f7ca7656ef9f462b722abc97725fb8e6897
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2022-09-04 14:20:02 +0100

    add loading to featured component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -10,15 +10,23 @@ import CardList from '../CardList/CardList';
 import './Featured.scss';
 
 const Featured = memo(({ mediaType, headTitle }) => {
-  const [shows, setShows] = useState('');
+  const [shows, setShows] = useState([]);
   const [active, setActive] = useState('day');
+  const [loading, setLoading] = useState(false);
 
   const toggleTrending = useCallback(
     async (timeUrl = 'day') => {
-      const res = await fetchFeatured(`trending/${mediaType}/${timeUrl}`);
-      const { data } = res;
-      setShows(data);
-      setActive(timeUrl);
+      setLoading(true);
+      try {
+        const res = await fetchFeatured(`trending/${mediaType}/${timeUrl}`);
+
+        setShows(res.data.results);
+        setActive(timeUrl);
+      } catch (error) {
+        console.log('error', error);
+      } finally {
+        setLoading(false);
+      }
     },
     [setShows, mediaType]
   );
@@ -27,72 +35,69 @@ const Featured = memo(({ mediaType, headTitle }) => {
     toggleTrending();
   }, [toggleTrending]);
 
-  if (!shows) {
+  if (loading) {
     return <Loading />;
   }
-  return (
-    <>
-      {!shows ? (
-        <Loading />
-      ) : (
-        <section className="section-featured">
-          <div className="titles">
-            <h1 id="headTitle">{headTitle}</h1>
-            <div className="timeButtons">
-              {/* eslint-disable-next-line */}
-              <div
-                id="btnDay"
-                type="button"
-                className={`${active === 'day' ? 'active' : ''}`}
-                onClick={() => toggleTrending('day')}
-              >
-                <p>day</p>
-              </div>
-              {/* eslint-disable-next-line */}
-              <div
-                id="btnWeek"
-                type="button"
-                className={`${active === 'week' ? 'active' : ''}`}
-                onClick={() => toggleTrending('week')}
-              >
-                <p>week</p>
-              </div>
-            </div>
-          </div>
-          <div className="container-featured">
-            {shows.results.map(
-              ({
-                id,
-                title,
-                name,
-                poster_path: imagePath,
-                release_date: releaseDate,
-                first_air_date: firstAirDate,
-                vote_average: rating,
-                media_type: media,
-              }) => {
-                const image =
-                  imagePath === null
-                    ? 'https://ctkbiotech.com/wp/wp-content/uploads/2018/03/not-available.jpg'
-                    : `${smallImageUrl}${imagePath}`;
 
-                return (
-                  <CardList
-                    key={Math.random()}
-                    id={id}
-                    image={image}
-                    title={title || name}
-                    releaseDate={releaseDate || firstAirDate}
-                    rating={rating}
-                    link={`/shows/${media}/${id}`}
-                  />
-                );
-              }
-            )}
+  return (
+    <section className="section-featured">
+      <div className="titles">
+        <h1 data-testid="header" id="headTitle">
+          {headTitle}
+        </h1>
+        <div className="timeButtons">
+          {/* eslint-disable-next-line */}
+          <div
+            id="btnDay"
+            type="button"
+            className={`${active === 'day' ? 'active' : ''}`}
+            onClick={() => toggleTrending('day')}
+          >
+            <p>day</p>
           </div>
-        </section>
-      )}
-    </>
+          {/* eslint-disable-next-line */}
+          <div
+            id="btnWeek"
+            type="button"
+            className={`${active === 'week' ? 'active' : ''}`}
+            onClick={() => toggleTrending('week')}
+          >
+            <p>week</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-featured">
+        {shows.map(
+          ({
+            id,
+            title,
+            name,
+            poster_path: imagePath,
+            release_date: releaseDate,
+            first_air_date: firstAirDate,
+            vote_average: rating,
+            media_type: media,
+          }) => {
+            const image =
+              imagePath === null
+                ? 'https://ctkbiotech.com/wp/wp-content/uploads/2018/03/not-available.jpg'
+                : `${smallImageUrl}${imagePath}`;
+
+            return (
+              <CardList
+                key={Math.random()}
+                id={id}
+                image={image}
+                title={title || name}
+                releaseDate={releaseDate || firstAirDate}
+                rating={rating}
+                link={`/shows/${media}/${id}`}
+              />
+            );
+          }
+        )}
+      </div>
+    </section>
   );
 });
 
